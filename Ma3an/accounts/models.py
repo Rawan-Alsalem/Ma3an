c9901ce983565044c986129bace3450ba91be56e
Norah <norahwasr@gmail.com>
2025-12-18 20:50:36 +0300

    initial accounts app

--- Ma3an/accounts/models.py
+++ Ma3an/accounts/models.py
@@ -1,3 +1,83 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    email = models.EmailField(unique=True, blank=False)
+
+    USERNAME_FIELD = 'email' 
+
+    REQUIRED_FIELDS = ['username']
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='accounts_user_set',
+        blank=True,
+        help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.',
+        related_query_name='user',
+    )
+
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='accounts_user_permissions_set',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        related_query_name='user',
+    )
+    
+    ROLE_CHOICES = (
+        ("traveler", "Traveler"),
+        ("agency", "Agency"),
+        ("supervisor", "Supervisor"),
+    )
+
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+
+    class Meta:
+        db_table = "CustomUser"
+
+
+class Traveler(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    passport_number = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Agency(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    agency_name = models.CharField(max_length=255)
+    license_number = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=50)
+    address = models.TextField()
+    city = models.CharField(max_length=100)
+    country = models.CharField(max_length=100)
+    commercial_license = models.CharField(max_length=100)
+    status = models.CharField(max_length=20, choices=(("Pending","Pending"),("Approved","Approved")), default="Pending")
+
+    def __str__(self):
+        return self.agency_name
+    
+    
+class TourGuide(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    agency = models.ForeignKey(Agency, on_delete=models.CASCADE, related_name="tour_guides")
+
+    # full_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=50, blank=True)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.full_name
+
+
+class PrivacySettings(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="privacy_settings")
+    show_email = models.BooleanField(default=True, help_text="Allow others to see your email")
+    # show_bio = models.BooleanField(default=True, help_text="Allow others to see your bio")
+    # show_portfolio = models.BooleanField(default=True, help_text="Allow others to see your portfolio")
+
+    def str(self):
+        return f"Privacy Settings for {self.user.username}"
--- Ma3an/accounts/views.py
+++ Ma3an/accounts/views.py
@@ -1,3 +1,95 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from .models import Traveler, Agency
+from django.contrib import messages
 
 # Create your views here.
+
+def signup_view(request : HttpRequest):
+    
+    if request.method == "POST":
+        role = request.POST.get("role")
+
+        # إنشاء User
+        user = User.objects.create_user(
+            username=request.POST["username"],
+            password=request.POST["password"],
+            email=request.POST["email"]
+        )
+
+        if role == "traveler":
+            Traveler.objects.create(
+                user=user,
+                first_name=request.POST.get("first_name", ""),
+                last_name=request.POST.get("last_name", "")
+            )
+
+        elif role == "agency":
+            Agency.objects.create(
+                user=user,
+                agency_name=request.POST.get("agency_name"),
+                license_number=request.POST.get("license_number"),
+                phone_number=request.POST.get("phone_number"),
+                address=request.POST.get("address"),
+                city=request.POST.get("city"),
+                country=request.POST.get("country"),
+                commercial_license=request.POST.get("commercial_license"),
+                status="Pending"
+            )
+
+        messages.success(request, "Account created successfully")
+        return redirect("accounts:signin_view")
+    return render(request, "accounts/signup_role.html")
+
+
+# def signup_traveler_view(request : HttpRequest):
+    
+#     if request.method == "POST":
+#         try:
+#             new_user = User.objects.create_user(
+#                 username = request.POST["username"],
+#                 password = request.POST["password"],
+#                 email = request.POST["email"],
+#                 first_name = request.POST["first_name"],
+#                 last_name = request.POST["last_name"],
+#             )
+
+#             new_user.save()
+
+#             messages.success(request, "registered successfully", "alert-success")
+#             return redirect("accounts:signin_view")
+#         except Exception as e: 
+#             print(e)
+    
+#     return render(request, "accounts/signup.html")
+
+
+# def signup_agency_view(request : HttpRequest):
+#     pass
+
+# def signup_guide_view(request : HttpRequest):
+#     pass
+        
+def signin_view(request : HttpRequest):
+    
+    if request.method == "POST":
+        user = authenticate(request, username = request.POST["username"],
+                password = request.POST["password"])
+        
+        if user:
+            login(request, user)
+            messages.success(request, "logged in successfuly", "alert-success")
+            return redirect(request.GET.get("next", "/"))
+        else:
+            messages.error(request, "Please try again, your info is wrong", "alert-danger")
+     
+    return render(request, "accounts/signin.html", {})
+
+
+def log_out_view(request : HttpRequest):
+    
+    logout(request)
+    messages.success(request, "logged out successfuly", "alert-success")
+    return redirect("main:home_view")
